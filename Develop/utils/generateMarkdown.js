83ab3d83b1bad80dc07ea374b3ecaf4c355eb756
Bugs <melanietorres425@live.com>
2020-08-20 00:00:39 -0400

    fixed questions and readme style

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,29 +2,31 @@
 function generateMarkdown(data) {
   return `# ${data.title}
   ![GitHub](https://img.shields.io/github/license/${data.github}/${data.title})
+
+  ## Description <a name="description"></a>
+  - ${data.description}
   ## Table of Contents
   1. [Description] (#Description)
-  [Installation Instructions](#Installation)
-  [Usage Information](#usage)
-  2. [Test Instructions](#test)
-  3. [Contribution Guideline](#contributing)
+  2. [Installation Instructions](#Installation)
+  3. [Usage Information](#usage)
   4. [License](#license)
-  5. [Questions](#questions)
+  5. [Contribution Guideline](#contributing)
+  6. [Test Instructions](#test)
+  7. [Questions](#questions)
  
-  ### Description <a name="description"></a>
-  - ${data.description}
+ 
   ### Installation Instructions <a name="installation"></a>
   - ${data.installation}
   ### Usage Information <a name="Usage"></a>
   - ${data.usage}
-  ## Contribution Guideline <a name="contribution"></a>
-  - ${data.contributing}
-  ## Test Instructions <a name="instructions"></a>
-  - ${data.instructions}
- 
   ## License <a name="license"></a>
   - ${data.badge}
+  ## Contribution Guideline <a name="contribution"></a>
+  - ${data.contributing}
+  ## Test Instructions <a name="tests"></a>
+  - ${data.tests}
   ## Questions <a name="questions"></a>
+  Any questions? Please Contact Below
   - GitHub Link
   [${data.github}](https://github.com/${data.github}) 
   - Email
